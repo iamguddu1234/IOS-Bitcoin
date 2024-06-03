afdb09c7cd8eddf191fb3c975c85b68526095da1
Akshay Bhamse <iamguddu37@gmail.com>
2024-06-03 16:53:08 +0530

    Initial Commit

---
+++ Bitcoin/BitcoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinApp.swift
+//  Bitcoin
+//
+//  Created by Akshay Bhasme on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
